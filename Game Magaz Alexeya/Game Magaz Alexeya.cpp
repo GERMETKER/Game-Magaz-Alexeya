8501fb0cd81a38ca6946cdee15bf01d87be2a90a
GERMETKER <160734087+GERMETKER@users.noreply.github.com>
2024-04-08 18:34:26 +0500

    Update Game Magaz Alexeya.cpp

--- Game Magaz Alexeya/Game Magaz Alexeya.cpp
+++ Game Magaz Alexeya/Game Magaz Alexeya.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iomanip>
 #include <limits>
+#include <cmath>
 //#include <Windows.h>
 // Игровой Магаз Алексея
 // Функции
@@ -11,28 +12,62 @@ void CreateStore();
 template <typename ArrType>
 void FillArr(ArrType statArr[], ArrType dinArr[], int size);
 void Shop();
-
+void ShopStorage();
+void Selling();
 
 
 //тестовые функции
 template <typename ArrType>
 void PrintArr(ArrType arr[], int size);
-// Глобальные массивы
+// Размеры
 int size = 10;
+int receiptSize = 1;
+// Глобальные массивы склада
 int* idArr = new int[size];
 std::string* nameArr = new std::string[size];
 int* countArr = new int[size];
 float* priceArr = new float[size];
+// Глобальные массивы чека
+std::string* nameReceiptArr = new std::string[receiptSize];
+int* countReceiptArr = new int[receiptSize];
+double* priceReceiptArr = new double[receiptSize];
+
+
+
+/*
+    inline int max(int a, int b)
+    {
+        return a > b ? a : b;
+    }
+    std::cout << max(10, 15) << "\n";
+    */
+    /*
+    std::cout << sqrt(25) << "\n";
+    std::cout << tan(25) << "\n";
+    std::cout << sin(25) << "\n";
+    std::cout << cos(25) << "\n";
+    std::cout << floor(25,6) << "\n";
+    #include <cmath>
+    */
+    //CreateStore();
+    //ShopStorage();
 int main()
 {
     setlocale(LC_ALL, "ru");
     srand(time(NULL));
-    Start();
-    return 0;
+
+    CreateStore();
+    ShopStorage();
+    Selling();
+    //Start();
     delete[]idArr;
     delete[]nameArr;
     delete[]countArr;
     delete[]priceArr;
+    delete[]nameReceiptArr;
+    delete[]countReceiptArr;
+    delete[]priceReceiptArr;
+    return 0;
 }
 void Start()
 {
@@ -57,7 +92,7 @@ void Start()
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             if (choose == 2)
             {
-                exit = true;              
+                exit = true;
             }
         }
         else
@@ -82,14 +117,13 @@ void Start()
     } while (!exit);
 }
 void CreateStore()
-
 {
     const int size_pwgood = 10;
     int id[size_pwgood]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::string name[size_pwgood]{"Devil May Cry 5 (PS4)", "Rezident Evil 4 (PS4)",
-        "Cyberpunk 2077 (PS4)", "GTA V (PS4)", "Detroit: Become Human (PS4)", "Far Cry 3 (PS4)",
-        "Red Dead Redemption 2 (PS4)", "Hogwartz Legacy (PS4)", "Minecraft (PS4)", "Terraria (PS4)"};
-    int count[size_pwgood]{9, 10, 10, 10, 8, 12, 10, 10, 11};
+    std::string name[size_pwgood]{"Devil May Cry 5 (PS4)      ", "Rezident Evil 4 (PS4)      ",
+        "Cyberpunk 2077 (PS4)       ", "GTA V (PS4)                ", "Detroit: Become Human (PS4)", "Far Cry 3 (PS4)            ",
+        "Red Dead Redemption 2 (PS4)", "Hogwartz Legacy (PS4)      ", "Minecraft (PS4)            ", "Terraria (PS4)             "};
+    int count[size_pwgood]{9, 10, 10, 10, 8, 12, 10, 10, 11, 9};
     float price[size_pwgood]{2999.99, 3799.55, 2899.44, 3099.99, 2756.86,
         2673.49, 3300.79, 4700.39, 4990.64, 2739.99};
     FillArr(id, idArr, size);
@@ -133,11 +167,11 @@ void Shop()
         } while (choose < 0 || choose > 6);
         if (choose == 1)
         {
-
+            ShopStorage();
         }
         else if (choose == 2)
         {
-
+            Selling();
         }
         else if (choose == 3)
         {
@@ -160,6 +194,61 @@ void Shop()
 
         }
     }
-
 }
-
+void ShopStorage()
+{
+    std::cout << "id товара\tНазвание\t\t\tЦена\t\tКол-во\n\n";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << idArr[i] + 1 << "\t\t" << nameArr[i] << "\t" << priceArr[i] << "\t\t" << countArr[i] << "\n";
+    }
+    std::cout << "\n\n";
+}
+void Selling()
+{
+    int chooseID, chooseCount, confirm;
+    while (true)
+    {
+        do
+        {
+            do
+            {
+                std::cout << "Введите ID товара, который хотите купить: \n";
+                std::cin >> chooseID;
+                if (chooseID < 1 || chooseID > size)
+                {
+                    std::cerr << "Нет такого ID\n";
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+            do
+            {
+                std::cout << "Вы выбрали: " << nameArr[chooseID - 1] << "\n";
+                std::cout << "Кол-во товара на складе: " << countArr[chooseID - 1] << "\n";
+                std::cout << "Введите кол-во товара:\n";
+                std::cin >> chooseCount;
+                if (chooseCount < 1 || chooseCount > countArr[chooseID - 1])
+                {
+                    std::cerr << "Товаров на складе не хватает\n";
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+            std::cout << "Вы выбрали: " << nameArr[chooseID - 1] << "\n";
+            std::cout << "В кол-ве: " << chooseCount << " шт\n";
+            std::cout << "Подтвердите выбор: \n1 - подтвердить \n2 - повторить\n";
+            std::cin >> confirm;
+            if (confirm == 1)
+            {
+                break;
+            }
+        } while (true);
+    }
+}
